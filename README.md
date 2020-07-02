40735a8d3658fa0d17ed52f9a683c9e859f56cd9
Min RK <benjaminrk@gmail.com>
2020-07-02 14:20:58 +0200

    exercise some more builds

--- README.md
+++ README.md
@@ -2,11 +2,32 @@
 
 Sampling repositories built with repo2docker.
 
-Scripts to check:
+
+Install:
+
+    pip install git+https://github.com/minrk/repo2docker-checker
+
+Check a repo:
+
+    repo2docker-checker binder-examples/requirements
+
+
+Our goal is to make some scripts to check:
 
 - does it build?
 - is there something to run?
 - does it run?
 - ...why?
+
+Currently, the output for each run is stored in a csv in `repo/results/results-...csv`.
+
+For now, we only have notebooks as tests, run with `nbconvert --execute` (the Python equivalent, anyway).
+
+Each test row consists of:
+
+- a test 'kind' (build or notebook),
+- boolean 'success',
+- a path relative to the run directory containing a log file for details (mostly interesting for failures).
+- additional metadata such as the repo, ref, commit date, repo2docker version, etc.
 
 This is a work in progress, summer research project at Simula Research Laboratory with @Vildeeide.
--- tests/test_checker.py
+++ tests/test_checker.py
@@ -1,11 +1,64 @@
-import repo2docker_checker
+import pytest
+
+from repo2docker_checker.checker import build_repo
+from repo2docker_checker.checker import clone_repo
+from repo2docker_checker.checker import main
+
+example_repo_short = "binder-examples/requirements"
+example_repo = f"https://github.com/{example_repo_short}"
+example_ref_passes = "11cdea057c300242a30e5c265d8dc79f60f644e1"
+example_ref_fails = "d37d5723fb410dbfb7fa3f470ac8bc211c5e6718"
+
+
+def test_build_succeeds(tmpdir):
+    repo = example_repo
+    ref = example_ref_passes
+    build_log_file = tmpdir.join("build-log.txt")
+    checkout_path, resolved_ref, timestamp = clone_repo(repo, ref)
+    assert ref.startswith(resolved_ref)
+    image_id, checkout_path = build_repo(
+        repo, resolved_ref, checkout_path, str(build_log_file), force_build=True
+    )
+    assert build_log_file.exists()
+    with build_log_file.open() as f:
+        assert "Successfully tagged" in f.read()
+
+    # second build should be skipped
+    build_log_file = tmpdir.join("build-log-reuse.txt")
+    image_id, checkout_path = build_repo(
+        repo, resolved_ref, checkout_path, str(build_log_file), force_build=False
+    )
+    with build_log_file.open() as f:
+        assert "already built" in f.read()
+
+    # force should trigger rebuild
+    build_log_file = tmpdir.join("build-log-force.txt")
+    image_id, checkout_path = build_repo(
+        repo, resolved_ref, checkout_path, str(build_log_file), force_build=True
+    )
+    with build_log_file.open() as f:
+        assert "Successfully tagged" in f.read()
+
+
+def test_build_fails(tmpdir):
+    repo = "https://github.com/minrk/repo2docker-checker"
+    ref = "build-fails"
+    build_log_file = tmpdir.join("build-log.txt")
+    checkout_path, resolved_ref, timestamp = clone_repo(repo, ref)
+    assert resolved_ref.startswith("a68ea7")
+    assert timestamp == "2020-07-01T14:23:17+02:00"
+    with pytest.raises(Exception):
+        image_id, checkout_path = build_repo(
+            repo, resolved_ref, checkout_path, str(build_log_file)
+        )
+    assert build_log_file.exists()
+    with build_log_file.open() as f:
+        assert "No matching distribution" in f.read()
 
 
 def test_example_passes():
-    repo2docker_checker.main(["binder-examples/requirements"])
+    main([f"{example_repo}@{example_ref_passes}"])
 
 
 def test_example_fails():
-    repo2docker_checker.main(
-        ["binder-examples/requirements@d37d5723fb410dbfb7fa3f470ac8bc211c5e6718"]
-    )
+    main([f"{example_repo_short}@{example_ref_fails}"])
